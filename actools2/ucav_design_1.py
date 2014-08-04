fa44f51d93bff606cc03e5405eb985565be2e637
tyan.maxim@gmail.com <tyan.maxim@gmail.com@d39d3ab0-4675-f461-a511-7731cff91ffd>
2014-08-04 13:47:20 +0000

    function for sensitivity analysis added

--- actools2/ucav_design_1.py
+++ actools2/ucav_design_1.py
@@ -21,7 +21,7 @@ class DesignFormulation(design.Design):
         self.xCurrent = np.zeros(len(self.x0))
         self.bnds = np.array([[l,u] for l,u in zip(self.lb,self.ub)])
         # --- constraints ---
-        self.WemptyMax      = 3533.0
+        self.WemptyMax      = 3500.0
         self.CnbMin         = 0.0
         self.ClbMax         = -0.05
         self.SMmin          = -0.10
@@ -43,7 +43,7 @@ class DesignFormulation(design.Design):
             self.xCurrent = x
             self._upd_configuration(x)
             try:
-                self._upd_analysis(x)
+                self._upd_analysis()
             except ValueError:
                 print x
                 print self.g(self.xCurrent)
@@ -69,7 +69,7 @@ class DesignFormulation(design.Design):
         self.set_taper_ratio_by_index(TR2,1)
         self.set_twist_by_index(twist,1)
     
-    def _upd_analysis(self,x):
+    def _upd_analysis(self):
         # update mass and balance
         # update drag
         self._update_mass()
@@ -139,6 +139,40 @@ class DesignFormulation(design.Design):
         g[3] *= 1.0e1
         g[4] *= 1.0e1
         return g
+    
+    def _set_dvar(self,x):
+        sweep1 = x[0]
+        sweep2 = x[1]
+        cr     = x[2]
+        TR1    = x[3]
+        TR2    = x[4]
+        l1     = x[5]
+        l2     = x[6]
+        twist1 = x[7]
+        twist2 = x[8]
+        c2 = cr*TR1
+        c3 = c2*TR2
+        self.set_chord_by_index(cr,0)
+        self.set_chord_by_index(c2,1)
+        self.set_chord_by_index(c3,2)
+        self.set_spans([l1,l2])
+        self.set_sweep_angles([sweep1,sweep2])
+        self.set_twist_by_index(twist1,0)
+        self.set_twist_by_index(twist2,1)
+    
+    def run_full_analysis(self,x):
+        self._set_dvar(x)
+        self._upd_analysis()
+        g = np.zeros(8)
+        g[0] = self.mass.empty()
+        g[1] = self.aero.derivs.Cnb
+        g[2] = self.aero.derivs.Clb
+        g[3] = self.aero.SM
+        g[4] = self.R
+        g[5] = self.Rcombat
+        g[6] = self.RC
+        g[7] = self.Vmax
+        return g
 
 
 def run_optimization():
@@ -156,6 +190,22 @@ def run_optimization():
     print ac._cnstrData
     ac.display()
 
+def function_for_sensitivity():
+    lb = np.array([40, 40, 6, 0.5, 0.15, 1, 3, -4, -4])
+    ub = np.array([60, 60, 7.5, 0.7, 0.35, 1.8, 3.2, 0, 0])
+    x0 = np.array([55, 55, 6.91, 0.6006, 0.2656, 1.443, 3.112, 0, -3])
+    
+    ac = DesignFormulation()
+    ac.load_xls('Baseline1')
+    ac.setup()
+    
+    ac._set_dvar(lb)
+    ac.display()
+    ac._set_dvar(ub)
+    ac.display()
+    ac._set_dvar(x0)
+    ac.display()
+
 
 if __name__=="__main__":
-    run_optimization()
+    function_for_sensitivity()
